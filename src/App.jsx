6392dc22195077e304fa236094c9bb072ffc0293
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-14 18:59:00 +0600

    routes and layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Routes/Routes'
 
 function App() {
   
 
   return (
     <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   )
 }
